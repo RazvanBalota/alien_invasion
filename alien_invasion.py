b74b3b3b5c43c97f5fdb343ee0f21abefa7be518
RazvanBalota <razvanbalota02@gmail.com>
2026-01-28 17:38:35 +0200

    feat: fire bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion: 
     """Overall class to manage game assets and behavior"""
@@ -19,6 +20,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         self.bg_color = (230, 230, 230)
 
@@ -27,6 +29,8 @@ class AlienInvasion:
         while True: 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._update_bullet()
             self._update_screen()
             self.clock.tick(60)
 
@@ -48,6 +52,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
         """Respont to key releases"""
@@ -56,9 +62,22 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullet(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+    
     def _update_screen(self):
         """Update images on the screen and filp to the new screen"""
         self.screen.fill(self.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
