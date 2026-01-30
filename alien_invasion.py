0afd836c18cda3c83df66d8804330ef46cdb9d3b
RazvanBalota <razvanbalota02@gmail.com>
2026-01-30 12:50:22 +0200

    full fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,7 @@ class AlienInvasion:
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
-        
+
         self._create_fleet()
         self.bg_color = (230, 230, 230)
 
@@ -78,7 +78,24 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet"""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            current_x = alien_width
+            current_y += 2 * alien_height
+    
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
     
     def _update_screen(self):
         """Update images on the screen and filp to the new screen"""
