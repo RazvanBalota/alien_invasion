a6234502b2f8f260934421990c51a14aa390b7ae
RazvanBalota <razvanbalota02@gmail.com>
2026-02-02 17:22:40 +0200

    fix: speedup

--- alien_invasion.py
+++ alien_invasion.py
@@ -67,6 +67,7 @@ class AlienInvasion:
         """Start game"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            self.settings.initialize_dnyamic_settings()
             self.stats.reset_stats()
             self.game_active = True
 
@@ -112,6 +113,7 @@ class AlienInvasion:
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Update position of fleet"""
